478c8ec7c28226741fbc9713df6d853b6476fedc
Ramsharan Rangarajan <r.ramsharan@gmail.com>
2022-10-20 11:36:24 +0530

    bug fix in merging.

--- src/manager/test/test_Manager.cpp
+++ src/manager/test/test_Manager.cpp
@@ -6,6 +6,12 @@ int main()
 {
   vm::Manager manager("coordinates.dat", "connectivity.dat");
   manager.write("mesh.off");
-  manager.write_bad_angles("bad_angles.off", 10.);
+  manager.write_bad_angles("bad_angles.off", 20.);
   manager.write_bad_vertices("bad_edges.off", 0.1);
+
+  // merge
+  manager.merge(20.);
+  manager.write("merged.off");
+  manager.write_bad_vertices("merged_bad_edges.off", 0.1);
+    
 }
--- src/manager/vm_manager_merge.cpp
+++ src/manager/vm_manager_merge.cpp
@@ -31,11 +31,11 @@ namespace vm
       {
 	// pop the first face
 	auto& face = bad_faces.begin()->first;
-
+	  
 	// is this still a valid face
 	if(mesh.is_valid(face) && !mesh.is_deleted(face))
 	  merge_face(face);
-
+	
 	bad_faces.erase(bad_faces.begin());
       }
 
@@ -53,42 +53,42 @@ namespace vm
     double best_quality = -1.;
     auto halfedge_circulator = mesh.halfedges(face);
     for(auto h:halfedge_circulator)
-      if(!mesh.is_boundary(h))
-	{
-	  auto nb_h    = mesh.opposite_halfedge(h);
-	  auto nb_face = mesh.face(nb_h);
-	  assert(mesh.is_valid(nb_face) && !mesh.is_deleted(nb_face));
+      {
+	auto nb_h    = mesh.opposite_halfedge(h);
+	auto nb_face = mesh.face(nb_h);
+	if(mesh.is_valid(nb_h) && mesh.is_valid(nb_face) && !mesh.is_deleted(nb_face))
+	  {
+	    // vertices of the new face created by merging face/nb_face
+	    std::vector<pmp::Point> verts{};
 
-	  // vertices of the new face created by merging face/nb_face
-	  std::vector<pmp::Point> verts{};
-
-	  // vertices from the face of h
-	  verts.push_back(mesh.position(mesh.to_vertex(h)));
-	  auto it_h = mesh.next_halfedge(h);
-	  while(it_h!=h)
-	    {
-	      verts.push_back(mesh.position(mesh.to_vertex(it_h)));
-	      it_h = mesh.next_halfedge(it_h);
-	    }
+	    // vertices from the face of h
+	    verts.push_back(mesh.position(mesh.to_vertex(h)));
+	    auto it_h = mesh.next_halfedge(h);
+	    while(it_h!=h)
+	      {
+		verts.push_back(mesh.position(mesh.to_vertex(it_h)));
+		it_h = mesh.next_halfedge(it_h);
+	      }
 	  
-	  // vertices from the face of nb_h
-	  it_h = mesh.next_halfedge(nb_h);
-	  while(it_h!=nb_h)
-	    {
-	      verts.push_back(mesh.position(mesh.to_vertex(it_h)));
-	      it_h = mesh.next_halfedge(it_h);
-	    }
-	  verts.pop_back();
+	    // vertices from the face of nb_h
+	    it_h = mesh.next_halfedge(nb_h);
+	    while(it_h!=nb_h)
+	      {
+		verts.push_back(mesh.position(mesh.to_vertex(it_h)));
+		it_h = mesh.next_halfedge(it_h);
+	      }
+	    verts.pop_back();
 
-	  // quality of the candidate merged face
-	  assert(inspect_face(verts)==true);
-	  double quality = face_quality(verts);
-	  if(quality>best_quality)
-	    {
-	      best_quality = quality;
-	      best_h       = h;
-	    }
-	}
+	    // quality of the candidate merged face
+	    assert(inspect_face(verts)==true);
+	    double quality = face_quality(verts);
+	    if(quality>best_quality)
+	      {
+		best_quality = quality;
+		best_h       = h;
+	      }
+	  }
+      }
 
     // there should be at least one candidate
     assert(best_quality>0. && mesh.is_valid(best_h));
