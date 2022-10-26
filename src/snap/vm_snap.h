7e92c33e7d7aff762d74b424019a013469933f7a
Ramsharan Rangarajan <r.ramsharan@gmail.com>
2022-10-26 16:33:41 +0530

    minor.

--- src/snap/vm_snap.h
+++ src/snap/vm_snap.h
@@ -7,8 +7,7 @@
 namespace vm
 {
   // determine whether the vertex of a face needs to be snapped
-  std::vector<pmp::Vertex> needs_snap(pmp::SurfaceMesh& mesh, const pmp::Face& face,
-				      const double eps_dist_ratio);
+  std::vector<pmp::Vertex> needs_snap(pmp::SurfaceMesh& mesh, const pmp::Face& face, const double eps_dist_ratio);
   
   // identify the closest halfedge of a face to a given vertex
   pmp::Halfedge closest_halfedge(pmp::SurfaceMesh& mesh, const pmp::Face& face, const pmp::Vertex& vertex);
