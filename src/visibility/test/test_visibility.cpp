a7ab20add0f9ead14b1fd8b4ae5352f94ea561fa
Ramsharan Rangarajan <r.ramsharan@gmail.com>
2023-01-03 13:34:10 +0530

    minor.

--- src/visibility/test/test_visibility.cpp
+++ src/visibility/test/test_visibility.cpp
@@ -9,16 +9,26 @@
 int main()
 {
   // environment
-  const double coords[][2] = {{2.0, 1.3}, {2.4, 1.7}, {2.8, 1.8}, {3.7, 1.6},
-  			      {3.4, 2.0}, {4.1, 3.0}, {5.3, 2.6}, {5.4, 1.2}, {4.9, 0.8}, {2.9, 0.7}};
-  const int nVertices = 10;
-
+  const double coords[][2] =
+    {{70, 76},
+     {70, 74},
+     {72, 74},
+     {72.9206, 71.733},
+     {73.9048, 74.0952},
+     {73.9048, 76},
+     {73.5246, 76.4162},
+     {73.5724, 77.1908},
+     {72.7337, 77.8255},
+     {71.985, 76.9184},
+     {70.0952, 77.9048}};
+  const int nVertices = 11;
+  
   // guard
-  const double guard[] = {2.81,1.35};
+  const double guard[] = {72, 76};
   
   std::fstream pfile;
   pfile.open("env.dat", std::ios::out);
-  for(int n=0; n<10; ++n)
+  for(int n=0; n<nVertices; ++n)
     pfile << coords[n][0] << " " << coords[n][1] << std::endl;
   pfile << coords[0][0] << " " << coords[0][1] << std::endl;
   pfile << std::endl << guard[0] << " " << guard[1] << std::endl; 
@@ -28,15 +38,20 @@ int main()
   pmp::SurfaceMesh mesh;
   for(int n=0; n<nVertices; ++n)
     vertices.push_back(mesh.add_vertex(pmp::Point(coords[n][0],coords[n][1],0.)));
-
+  
   // guard location
   auto guard_vertex = mesh.add_vertex(pmp::Point(guard[0],guard[1],0.));
-
+  
   // add faces
-  mesh.add_face({guard_vertex, vertices[0], vertices[9]});
-  mesh.add_face({guard_vertex, vertices[9], vertices[8]});
-  mesh.add_face({guard_vertex, vertices[8], vertices[7], vertices[6], vertices[5], vertices[4], vertices[3], vertices[2], vertices[1], vertices[0]});
+  mesh.add_face({guard_vertex, vertices[0], vertices[1], vertices[2]}); 
+  mesh.add_face({guard_vertex, vertices[2], vertices[3], vertices[4]}); 
+  mesh.add_face({guard_vertex, vertices[4], vertices[5]});
+  mesh.add_face({guard_vertex, vertices[5], vertices[6], vertices[7]}); 
+  mesh.add_face({guard_vertex, vertices[7], vertices[8], vertices[9]});
+  mesh.add_face({guard_vertex, vertices[9], vertices[10]}); 
+  mesh.add_face({guard_vertex, vertices[10], vertices[0]});
   vm::write_off(mesh, "mesh.off");
+  vm::write_dat(mesh, "mesh.dat");
   
   // visibility polygon
   auto vis_poly_verts = vm::compute_visibility_polygon(mesh, guard_vertex);
