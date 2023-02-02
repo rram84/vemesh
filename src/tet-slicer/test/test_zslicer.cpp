5de4e4026cc59a40451fe421679af75765ec9fe5
Ramsharan Rangarajan <r.ramsharan@gmail.com>
2023-02-02 09:37:00 +0530

    slicing example modified for the case of a cube.

--- src/tet-slicer/test/test_zslicer.cpp
+++ src/tet-slicer/test/test_zslicer.cpp
@@ -3,23 +3,38 @@
 #include <vm_TetMesh.h>
 #include <iostream>
 #include <vm_io.h>
+#include <random>
 
 int main()
 {
-  
-  // Read the tet mesh
-  vm::TetMesh MD;
-  MD.read_tec("tetmesh.tec");
+  // generate a random z
+  std::random_device rd; 
+  std::mt19937 gen(rd()); 
+  std::uniform_real_distribution<> dis(-0.9, 0.9);
 
-  // perturb nodes away from z = 50
-  const double zcoord = 50.;
-  const double EPS    = 1.e-1;
-  int num_vert_perturbed = MD.zperturb(zcoord, EPS);
-  std::cout << "Perturbed " << num_vert_perturbed << " vertices away from the plane z = " << zcoord << std::endl;
-  MD.write_tec("zpert.tec");
+  for(int i=0; i<25; ++i)
+    {
+      std::cout << std::endl << std::endl;
+      
+      // Read the tet mesh
+      vm::TetMesh MD;
+      MD.read_tec("cube.tec");
   
-  // slice the mesh at z = zcoord
-  auto surf_mesh = MD.zslice(zcoord);
-  vm::write_off(surf_mesh, "slice.OFF");
+      // zcoord
+      const double zcoord = dis(gen);
+      std::cout << "Slicing at z-coordinate: " << zcoord << std::endl;
+      
+      // perturb nodes away from zcoord by a small tolerance 
+      const double EPS    = 1.e-2;
+      int num_vert_perturbed = MD.zperturb(zcoord, EPS);
+      std::cout << "Perturbed " << num_vert_perturbed << " vertices away from the plane z = " << zcoord << std::endl;
+      MD.write_tec("zslice-"+std::to_string(i) + ".tec");
+      
+      // slice the mesh at z = zcoord
+      auto surf_mesh = MD.zslice(zcoord);
+      vm::write_off(surf_mesh, "zslice-"+std::to_string(i)+".OFF");
+    }
+
+  // done
 }
   
