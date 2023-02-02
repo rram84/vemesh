00e9b9b9e06cf7adec3f638c46ebd64cab4cf117
Ramsharan Rangarajan <r.ramsharan@gmail.com>
2023-02-02 09:56:34 +0530

    adding a small tolerance to keep sample points away from the boundary of the visibility polygon.

--- src/sampling/vm_polygon_sampling.cpp
+++ src/sampling/vm_polygon_sampling.cpp
@@ -8,9 +8,10 @@ namespace vm
 {
   namespace bg  = boost::geometry;
   namespace bgm = boost::geometry::model;
-  using boost_point_t   = bgm::point<double, 2, bg::cs::cartesian>;
-  using boost_polygon_t = bgm::polygon<boost_point_t>;
-  using boost_box_t     = bgm::box<boost_point_t>;
+  using boost_point_t      = bgm::point<double, 2, bg::cs::cartesian>;
+  using boost_polygon_t    = bgm::polygon<boost_point_t>;
+  using boost_box_t        = bgm::box<boost_point_t>;
+  using boost_linestring_t = bgm::linestring<boost_point_t>;
   
   std::vector<std::pair<double,double>>
   compute_polygon_sampling(const std::vector<std::pair<double,double>>& vertices, const int num_points)
@@ -18,19 +19,31 @@ namespace vm
     // this polygon
     const int nVerts = static_cast<int>(vertices.size());
     assert(nVerts>=2);
-    boost_polygon_t poly;
+    boost_polygon_t    poly;
+    boost_linestring_t ls;
     for(auto& v:vertices)
-      bg::append(poly.outer(), boost_point_t(v.first,v.second));
+      {
+	bg::append(poly.outer(), boost_point_t(v.first,v.second));
+	bg::append(ls, boost_point_t(v.first,v.second));
+      }
 
     // repeat the first vertex
     bg::append(poly.outer(), boost_point_t(vertices.front().first, vertices.front().second));
+    bg::append(ls, boost_point_t(vertices.front().first, vertices.front().second));
     bg::correct(poly);
+    bg::correct(ls);
+    
+    // average edge length- will be used for normalization
+    const double avg_edge_len = bg::perimeter(poly)/static_cast<double>(nVerts);
 
+    // tolerance to fence sample points away from the boundary
+    const double BOUNDARY_FENCE_TOL = 0.025;
+    
     // axis aligned bounding box
     boost_box_t envelope;
     boost::geometry::envelope(poly, envelope);
 
-    // corners
+    // corners of the bounding box
     const auto& min_corner = envelope.min_corner();
     const auto& max_corner = envelope.max_corner();
     const double A[] = {bg::get<0>(min_corner), bg::get<1>(min_corner)};
@@ -52,16 +65,15 @@ namespace vm
 
 	// does this sample lie in the polygon?
 	if(bg::within(sample, poly)==true)
-	  {
-	    samples.push_back({bg::get<0>(sample), bg::get<1>(sample)});
-	    ++count;
-	  }
+	  if(bg::distance(sample, ls)/avg_edge_len > BOUNDARY_FENCE_TOL)
+	    {
+	      samples.push_back({bg::get<0>(sample), bg::get<1>(sample)});
+	      ++count;
+	    }
       }
     
     // done
     return std::move(samples);
   }
-
-
 
 }
