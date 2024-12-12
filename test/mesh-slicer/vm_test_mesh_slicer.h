80d90090187807eea07d59e8666b4a7d302989fa
Ramsharan Rangarajan <r.ramsharan@gmail.com>
2024-12-12 10:02:32 +0530

    minor.

--- test/mesh-slicer/vm_test_mesh_slicer.h
+++ test/mesh-slicer/vm_test_mesh_slicer.h
@@ -11,14 +11,15 @@ namespace vm
   namespace test
   {
     using LevelSetFunction_t = std::function<double(const double*)>;
-
-    int adjust_mesh_nodes(pmp::SurfaceMesh& mesh, const double phi_eps, const double pert_eps, LevelSetFunction_t& ls_func);
-  
+    
     void clip_mesh(pmp::SurfaceMesh& mesh, const double phi_eps, LevelSetFunction_t& lsfunc,
 		   const int mat_id, const int boundary_id);
 
     void embed_interface(pmp::SurfaceMesh& mesh, const double phi_eps, LevelSetFunction_t& lsfunc,
 			 const std::pair<int,int> domain_id, const int interface_id);
+	
+    int adjust_mesh_nodes(pmp::SurfaceMesh& mesh, const double phi_eps, const double pert_eps, LevelSetFunction_t& ls_func);
+  
 
     void prep_mesh(pmp::SurfaceMesh& mesh, const double phi_eps,
 		   LevelSetFunction_t& lsfunc,
