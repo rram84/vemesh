a8816fa6daaa1047710445ebbe29a30f9f7b8913
Ramsharan Rangarajan <r.ramsharan@gmail.com>
2026-01-02 17:02:26 +0530

    application example in progress.

--- app/vemesh.cpp
+++ app/vemesh.cpp
@@ -15,15 +15,26 @@
 // -n number of iterations to perform
 // -s number of vertex samples in case of vertex relaxations
 // -f quality improvement factor in case of agglomeration
+// -m face quality metric, 1:element stability ratio, 2:shape quality, 3:minimum angle
 // -v optional argument to print meshes after successive merges (within each iteration)
 
-#include <vm_Manager.h>
-#include <vm_quality.h>
+#include <vm_MeshOptimizer.h>
+#include <vm_face_quality.h>
 #include <vm_io.h>
 #include <CLI/CLI.hpp>
 
+namespace fs = std::filesystem;
+
+// optimization option
+enum class OptOption {
+  agglomerate_only,
+  relax_only,
+  agglomerate_then_relax,
+  relax_then_agglomerate
+};
+
 // validate command line options
-std::map<std::string, CLI::Option*> validate_CLI_options(CLI::App &app);
+std::tuple<OptOption, bool> validate_CLI_options(CLI::App &app);
 
 // output directory management
 void manage_output_directory(const std::string outdir);
@@ -44,9 +55,9 @@ int main(int argc, char **argv)
   app.footer("Sample usage: \
              \n=================== \
              \n(i)   agglomerate elements:  ./vemesh -a -i in_mesh.OFF -o out_dir -n 5 -f 1.2 -v \
-             \n(ii)  relax vertices:        ./vemesh -r -i in_mesh.OFF -o out_dir -n 5 -s 25  \
-             \n(iii) agglomerate and relax: ./vemesh --ar -i in_mesh.OFF -o out_dir -n 5 -f 1.2 -s 25 -v \
-             \n(iv)  relax and agglomerate: ./vemesh --ra -i in_mesh.OFF -o out_dir -n 5 -f 1.2 -s 25  \n");
+             \n(ii)  relax vertices:        ./vemesh -r -i in_mesh.OFF -o out_dir -n 5 -s 5  \
+             \n(iii) agglomerate and relax: ./vemesh --ar -i in_mesh.OFF -o out_dir -n 5 -f 1.2 -s 5 -v \
+             \n(iv)  relax and agglomerate: ./vemesh --ra -i in_mesh.OFF -o out_dir -n 5 -f 1.2 -s 5  \n");
 
   // Flags
   app.add_flag("-a", "flag to agglomerate elements");
@@ -57,24 +68,26 @@ int main(int argc, char **argv)
   // Options
   std::string meshfile;    // input mesh
   std::string outdir;      // output directory
-  double qthresh;          // quality threshold
+  double qepsilon;         // quality threshold
   int  num_iters;          // iteration count
   bool vis_flag = false;   // detailed visualization
   int num_samples;         // number of sample points to generate
   double qfactor;          // element quality improvement factor
+  int metric_num;          // 0:stability ratio, 1:shape quality, 2:min angle
   app.add_option("-i", meshfile, "input mesh file in OFF/vtk format; should exist")->required()->check(CLI::ExistingFile);
   app.add_option("-o", outdir, "output directory; created if it does not exist; will be cleared if it does")->required();
   app.add_option("-n", num_iters, "number of iterations")->required()->check(CLI::PositiveNumber);
-  app.add_option("-q", qthresh, "lower bound for acceptable element quality; elements with poorer qualities are marked for improvement")->required()->check(CLI::PositiveNumber);
+  app.add_option("-q", qepsilon, "lower bound for acceptable element quality; elements with poorer qualities are marked for improvement")->required()->check(CLI::PositiveNumber);
   app.add_option("-f", qfactor, "minimum factor of improvement in element quality for agglomeration")->check(CLI::PositiveNumber);
   app.add_option("-s", num_samples, "number of random samples to generate for vertex relaxation")->check(CLI::PositiveNumber);
-  app.add_flag("-v", vis_flag, "detailed mesh output after EVERY MESH UPDATE; leads to large num of file outputs; use sparingly");
+  app.add_option("-m", metric_num, "quality metric 1=element stability ratio, 2=shape quality, 3=min angle")->check(CLI::PositiveNumber);
+  app.add_flag("-v", vis_flag, "detailed mesh output after every mesh update; leads to large num of file outputs; use sparingly");
 
   // parse options
   CLI11_PARSE(app, argc, argv);
 
   // validate  list of options
-  const auto option_map = validate_CLI_options(app);
+  const auto options = validate_CLI_options(app);
   app.parse(argc, argv);
 
   // expect at least one iteration
@@ -83,62 +96,95 @@ int main(int argc, char **argv)
   // output directory
   manage_output_directory(outdir);
 
-  // Manager
-  vm::Manager manager(meshfile);
+  // Input mesh
+  const std::string extension = fs::path(meshfile).extension().str();
+  pmp::SurfaceMesh in_mesh;
+  if(extension==".off" || extension==".OFF")
+    in_mesh = vm::read_off(meshfile);
+  else if(extension==".vtk" || extension==".VTK")
+    in_mesh = vm::read_vtk(meshfile);
+  else
+    throw std::runtime_error("Expected mesh format to be off/vtk, given "+meshfile);
+  
+  // Mesh optimizer
+  vm::MeshOptimizer optimizer(in_mesh);
+  const auto& mesh = optimizer.get_mesh();
+
+  // Face quality metric
+  static const std::array<vm::FaceQualityFn, 3> all_quality_metrics = {
+    vm::quality::vem_stability_ratio, // VEM element stability ratio
+    vm::quality::geom_shape_ration,   // shape quality metric
+    vm::quality::geom_min_angle       // min included angle
+  };
+  const auto face_quality_metric = all_quality_metrics[metric_num-1];
+
+  // Quality evaluator
+  vm::QualityEvaluator QE(face_quality_metric);
   
   // initial mesh quality
-  manager.compute_face_qualities(vm::FaceQuality::stiffness);
-  manager.write_mesh(outdir+"/vtk/init.vtk");
-  auto& mesh = manager.get_mesh();
-  vm::write_suku_format(mesh, outdir+"/suku/init");
+  const std::string qf_tag = "face_quality";
+  const std::string qv_tag = "vertex_quality";
+  optimizer.evaluate_face_qualities(qf_tag, QE);
+  optimizer.evaluate_vertex_qualities(qf_tag, qe_tag);
 
-  // face qualities
-  
+  // save mesh with qualities
+  vm::write_vtk(mesh, "input_mesh.vtk");
+
+  // options provided
+  const auto& opt_option = std::get<OptOption>(options);
+  const bool verbose_flag = std::get<bool>(options);
+
   for(int iter=0; iter<num_iters; ++iter) {
 
-    std::cout << "Iteration " << iter << std::endl;
+    std::cout << "Mesh improvement iteration " << iter <<":\n" << std::flush;
+
+    // optimize
+    switch(opt_option)
+      {
+      case OptOption::agglomerate_only:
+	{
+	  optimizer.agglomerate(QE, qepsilon, qfactor);
+	  break;
+	}
+
+      case OptOption::relax_only:
+	{
+	  optimizer.relax(QE, qepsilon, qfactor);
+	  break;
+	}
+
+      case OptOption::agglomerate_then_relax:
+	{
+	  optimizer.agglomerate(QE, qepsilon, qfactor);
+	  optimizer.relax(QE, qepsilon, qfactor);
+	  break;
+	}
+
+      case OptOption::relax_then_agglomerate:
+	{
+	  optimizer.relax(QE, qepsilon, qfactor);
+	  optimizer.agglomerate(QE, qepsilon, qfactor);
+	  break;
+	}
+      }
+    
     // callbacks
-    auto callback_a = !(*option_map.at("v")) ? vm::MeshUpdateCallback_f(nullptr) : std::bind(MeshUpdateCallback, outdir, iter, "a", std::placeholders::_1, std::placeholders::_2, std::placeholders::_3);
-    auto callback_r = !(*option_map.at("v")) ? vm::MeshUpdateCallback_f(nullptr) : std::bind(MeshUpdateCallback, outdir, iter, "r", std::placeholders::_1, std::placeholders::_2, std::placeholders::_3);
+    //auto callback_a = !(*option_map.at("v")) ? vm::MeshUpdateCallback_f(nullptr) : std::bind(MeshUpdateCallback, outdir, iter, "a", std::placeholders::_1, std::placeholders::_2, std::placeholders::_3);
+    //auto callback_r = !(*option_map.at("v")) ? vm::MeshUpdateCallback_f(nullptr) : std::bind(MeshUpdateCallback, outdir, iter, "r", std::placeholders::_1, std::placeholders::_2, std::placeholders::_3);
     
-    // agglomeration only
-    if(*option_map.at("a"))
-      manager.merge_faces(vm::FaceQuality::stiffness, qthresh, qfactor, callback_a);
-
-    // relaxation only
-    else if(*option_map.at("r"))
-      manager.move_vertices(vm::VertexQuality::shape, 1.-qthresh, num_samples, std::sqrt(num_samples), {1}, callback_r); // relax vertex with interface_id = 1
+    // save output    
+    optimizer.evaluate_face_qualities(QE, qf_tag);
+    optimizer.evaluate_vertex_qualities(qf_tag, qv_tag);
+    vm::write_vtk(mesh, "output.vtk");
     
-    // agglomeration->relaxation
-    else if(*option_map.at("ar")) {
-      // agglomerate
-      manager.merge_faces(vm::FaceQuality::stiffness, qthresh, qfactor, callback_a);
-      // relax
-      manager.move_vertices(vm::VertexQuality::shape, 1.-qthresh, num_samples, std::sqrt(num_samples), {1}, callback_r);
-    }
-
-    // relaxation->agglomeration
-    else if(*option_map.at("ra")) {
-      // relax
-      manager.move_vertices(vm::VertexQuality::shape, 1.-qthresh, num_samples, std::sqrt(num_samples), {1}, callback_r);
-      // agglomerate
-      manager.merge_faces(vm::FaceQuality::stiffness, qthresh, qfactor, callback_a);
-    }
-    
-    // save output at the end of this iteration
-    manager.compute_face_qualities(vm::FaceQuality::stiffness);
-    manager.write_mesh(outdir+"/vtk/mesh-iter-"+std::to_string(iter)+".vtk");
-    vm::write_suku_format(mesh, outdir+"/suku/mesh-iter-"+std::to_string(iter));
-    std::cout << std::endl;
-
     // inspect the mesh
-    manager.inspect_mesh();
+    //manager.inspect_mesh();
   }
 }
   
 
 // validate command line options
-std::map<std::string, CLI::Option*> validate_CLI_options(CLI::App& app) {
+std::tuple<OptOption, bool> validate_CLI_options(CLI::App &app) {
 
   // options
   auto options = app.get_options();
@@ -166,6 +212,7 @@ std::map<std::string, CLI::Option*> validate_CLI_options(CLI::App& app) {
   op->needs(option_map["i"]);
   op->needs(option_map["q"]);
   op->needs(option_map["n"]);
+  op->needs(option_map["m"]);
   
   // // agglomeration
   if(op_string=="a" || op_string=="ar" || op_string=="ra")
@@ -182,47 +229,52 @@ std::map<std::string, CLI::Option*> validate_CLI_options(CLI::App& app) {
   if(op_string=="r")
     op->excludes(option_map["f"]);
 
-  return option_map;
+  // Return the type of operation & callback verbosity
+  std::tuple<OptOption, bool> option;
+  auto& opt_option = std::get<OptOption>(option);
+  auto& verbose_flag = std::get<bool>(option);
+
+  if(*option_map.at("a"))
+    opt_option = OptOption::agglomerate_only;
+  else if(*option_map.at("r"))
+    opt_option = OptOption::relax_only;
+  else if(*option_map.at("ar"))
+    opt_option = OptOption::agglomerate_then_relax;
+  else if(*option_map.at("ra"))
+    opt_option = OptOption::relax_then_agglomerate;
+
+  if(*option_map.at("v"))
+    verbose_flag = true;
+  
+  return option;
 }
 
 
 // output directory management
 void manage_output_directory(const std::string outdir)
 {
-  namespace fs = std::filesystem;
   bool flag;
   
   // create the output directory if it does not exist, erase mesh files
   if(!fs::exists(outdir)) {
     flag = fs::create_directory(outdir); assert(flag);
-    }
+  }
   
-  // check for the vtk/ and suku subfolders
+  // check for the vtk subfolders
   bool has_vtk = false;
-  bool has_suku = false;
   for(const auto& entry : fs::directory_iterator(outdir)) {
     if(entry.is_directory() && entry.path().filename()=="vtk")
       has_vtk = true;
-    else if(entry.is_directory() && entry.path().filename()=="suku")
-      has_suku = true;
   }
   
   if(!has_vtk) {
     flag = fs::create_directory(outdir+"/vtk"); assert(flag);
-  }
-  if(!has_suku) {
-    flag = fs::create_directory(outdir+"/suku"); assert(flag);
   }
   
   // clear vtk files
   for (const auto& entry : fs::directory_iterator(outdir+"/vtk"))
     if (entry.is_regular_file() && entry.path().extension() == ".vtk")
       fs::remove(entry.path());
-  
-  // clear .node/.ele files
-  for (const auto& entry : fs::directory_iterator(outdir+"/suku"))
-    if (entry.is_regular_file() && (entry.path().extension() == ".node" || entry.path().extension() == ".ele"))
-      fs::remove(entry.path());
-  
+    
   return;
 }
